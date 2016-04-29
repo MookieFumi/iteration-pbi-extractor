6c4a50029430621a3b2f62b96a40d5597bc3b032
Miguel Angel Martin Hernandez <mookiefumi@outlook.com>
2016-04-29 18:14:20 +0200

    SoC. Separation of concerns

---
+++ js/app.index.js
@@ -0,0 +1,122 @@
+registerNS('app');
+
+app.index = (function() {
+    var config = {
+        regEx: new RegExp("\<(.*?)\>"),
+        registerHandlebarHelpers: function() {
+            Handlebars.registerHelper('is_pbi', function(item, match, options) {
+                if (item.workItemType === match) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+            Handlebars.registerHelper("fromDateToDate", function(startDate, endDate) {
+                if (startDate == null) {
+                    return 'No data';
+                }
+                return moment(startDate).format('DD/MM/YYYY') + ' - ' + moment(endDate).format('DD/MM/YYYY');
+            });
+        }
+    };
+    config.registerHandlebarHelpers();
+
+    var projects = {
+        append: function(data) {
+            var $projects = $("[data-role='projects']");
+            var $select = $projects.find("select");
+            $(data.value).each(function(index, element) {
+                $select.append($('<option>', {
+                    value: element.name,
+                    text: element.name
+                }));
+            });
+            $select.material_select();
+            $projects.removeClass('hide');
+        },
+        get: function() {
+            var fields = app.index.getFields();
+            $.when(app.vsoService.getProjects(fields.userName, fields.password))
+                .then(function(data, textStatus, jqXHR) {
+                    app.index.projects.append(data);
+                    iterations.get();
+                });
+        }
+    };
+
+    var iterations = {
+        append: function($iterations, element) {
+            var context = {
+                path: element.path,
+                name: element.name,
+                startDate: element.attributes.startDate,
+                finishDate: element.attributes.finishDate
+            };
+            var template = Handlebars.compile($("#iteration-template").html());
+            var html = template(context);
+            $iterations.append(html);
+        },
+        clear: function() {
+            var $iterations = $('[data-role="iterations"]');
+            $iterations.empty();
+        },
+        get: function() {
+            app.index.iterations.clear();
+            app.index.workItems.clear();
+            var fields = app.index.getFields();
+            var $iterations = $('[data-role="iterations"]');
+            app.vsoService.getIterations(fields.project, fields.userName, fields.password).done(function(data) {
+                $(data.value).each(function(index, element) {
+                    app.index.iterations.append($iterations, element);
+                });
+            });
+        }
+    };
+
+    var workItems = {
+        append: function($workItems, element) {
+            var assignedTo = element.fields["System.AssignedTo"];
+            var imageUrl = assignedTo == undefined ? 'http://www.gravatar.com/avatar' : 'http://www.gravatar.com/avatar/' + md5(config.regEx.exec(assignedTo)[1]);
+            var context = {
+                title: element.fields["System.Title"],
+                effort: element.fields["Microsoft.VSTS.Scheduling.Effort"],
+                workItemType: element.fields["System.WorkItemType"],
+                imageUrl: imageUrl
+            };
+            var template = Handlebars.compile($("#workitem-template").html());
+            var html = template(context);
+            $('[data-role="workItems"]').append(html);
+        },
+        clear: function() {
+            var $workItems = $('[data-role="workItems"]');
+            $workItems.empty();
+        },
+        get: function(path) {
+            app.index.workItems.clear();
+            var fields = app.index.getFields();
+            var $workItems = $('[data-role="workiIems"]');
+            app.vsoService.getWorkItems(fields.project, path, fields.userName, fields.password)
+                .done(function(data) {
+                    app.index.workItems.clear();
+                    $(data.value).each(function(index, element) {
+                        app.index.workItems.append($workItems, element);
+                    });
+                });
+        }
+    };
+
+    function getFields() {
+        return {
+            project: $("select").val(),
+            userName: $("[data-role='form']").find("input[name=user]").val(),
+            password: $("[data-role='form']").find("input[name=password]").val()
+        }
+    };
+
+    return {
+        iterations: iterations,
+        workItems: workItems,
+        projects: projects,
+        getFields: getFields
+    };
+}());
--- js/app.vsoService.js
+++ js/app.vsoService.js
@@ -9,113 +9,49 @@ app.vsoService = (function() {
             workItemsDetail: 'https://analyticalways.visualstudio.com/DefaultCollection/_apis/wit/workitems?api-version=2.0'
         }
     };
-    var regEx = new RegExp("\<(.*?)\>");
 
-    var registerHandlebarHelpers = (function() {
-        Handlebars.registerHelper('is_pbi', function(item, match, options) {
-            if (item.workItemType === match) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-        Handlebars.registerHelper("fromDateToDate", function(startDate, endDate) {
-            if (startDate == null) {
-                return 'No data';
-            }
-            return moment(startDate).format('DD/MM/YYYY') + ' - ' + moment(endDate).format('DD/MM/YYYY');
-        });
-    })();
-
-    var getAuthorizationHeader = function(userName, password) {
+    function getAuthorizationHeader(userName, password) {
         return window.btoa(userName + ":" + password);
     };
 
+    function showError(textStatus, error) {
+        var err = textStatus + ", " + error;
+        Materialize.toast('There was a problem retrieving data', 1500, 'rounded');
+    }
+
     var getProjects = function(userName, password) {
         var header = getAuthorizationHeader(userName, password);
-        $.when(app.ajax.get(config.url.project, header).done(function(data) {
-            var $projects = $("[data-role='projects']");
-            var $select = $projects.find("select");
-            $(data.value).each(function(index, element) {
-                $select.append($('<option>', { value: element.name, text: element.name }));
+        return app.ajax.get(config.url.project, header)
+            .fail(function(jqxhr, textStatus, error) {
+                showError(textStatus, error);
             });
-            $select.material_select();
-            $projects.removeClass('hide');
-        }).fail(function(jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            Materialize.toast('There was a problem retrieving data', 1500, 'rounded');
-        })).then(function (data, textStatus, jqXHR){
-            var project = $(data.value).first().prop("name");
-            app.vsoService.getIterations(project, userName, password);
-        });
     };
 
     var getIterations = function(project, userName, password) {
         var header = getAuthorizationHeader(userName, password);
-        app.ajax.get(config.url.iteration.replace('{0}', project), header).done(function(data) {
-            var $iterations = $('[data-role="iterations"]');
-            $iterations.empty();
-            var $workitems = $('[data-role="workItems"]');
-            $workitems.empty();
-            $(data.value).each(function(index, element) {
-                createIteration($iterations, element);
+        return app.ajax.get(config.url.iteration.replace('{0}', project), header)
+            .fail(function(jqxhr, textStatus, error) {
+                showError(textStatus, error);
             });
-        }).fail(function(jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            Materialize.toast('There was a problem retrieving data', 1500, 'rounded');
-        });
     };
 
-    var getWorkItems = function(project, userName, password) {
-        var $this = $(this);
-        var iterationPath = $this.data().path;
+    var getWorkItems = function(project, path, userName, password) {
         var data = JSON.stringify({
-            "query": "SELECT * FROM WorkItems WHERE [System.TeamProject] = '" + project + "' AND ([System.WorkItemType] = 'Product Backlog Item' OR [System.WorkItemType] = 'Bug') AND [System.IterationPath] = '" + iterationPath + "'"
+            "query": "SELECT * FROM WorkItems WHERE [System.TeamProject] = '" + project + "' AND ([System.WorkItemType] = 'Product Backlog Item' OR [System.WorkItemType] = 'Bug') AND [System.IterationPath] = '" + path + "'"
         });
         var url = config.url.workItemsInIteration.replace('{0}', project);
         var header = getAuthorizationHeader(userName, password);
-        $.when(app.ajax.post(url, header, data))
-            .then(function(data, textStatus, jqXHR) {
-                if (data.workItems.length == 0) {
-                    Materialize.toast('There are no data available', 1500, 'rounded');
-                    $('[data-role="workItems"]').empty();
-                    return;
-                }
-                var url = config.url.workItemsDetail + '&ids=' + _.pluck(data.workItems, 'id');
-                app.ajax.get(url, header).done(function(returnedData) {
-                    var $workitems = $('[data-role="workItems"]');
-                    $workitems.empty();
-                    $(returnedData.value).each(function(index, element) {
-                        createWorkItem($workitems, element);
-                    });
+
+        return $.when(app.ajax.post(url, header, data)
+            .fail(function(jqxhr, textStatus, error) {
+                showError(textStatus, error);
+            })).then(function(data, textStatus, jqXHR) {
+            var url = config.url.workItemsDetail + '&ids=' + _.pluck(data.workItems, 'id');
+            return app.ajax.get(url, header)
+                .fail(function(jqxhr, textStatus, error) {
+                    showError(textStatus, error);
                 });
-            });
-    };
-
-    var createIteration = function($iterations, element) {
-        var context = {
-            path: element.path,
-            name: element.name,
-            startDate: element.attributes.startDate,
-            finishDate: element.attributes.finishDate
-        };
-        var template = Handlebars.compile($("#iteration-template").html());
-        var html = template(context);
-        $iterations.append(html);
-    };
-
-    var createWorkItem = function($workitems, element) {
-        var assignedTo = element.fields["System.AssignedTo"];
-        var imageUrl = assignedTo == undefined ? 'http://www.gravatar.com/avatar' : 'http://www.gravatar.com/avatar/' + md5(regEx.exec(assignedTo)[1]);
-        var context = {
-            title: element.fields["System.Title"],
-            effort: element.fields["Microsoft.VSTS.Scheduling.Effort"],
-            workItemType: element.fields["System.WorkItemType"],
-            imageUrl: imageUrl
-        };
-        var template = Handlebars.compile($("#workitem-template").html());
-        var html = template(context);
-        $('[data-role="workItems"]').append(html);
+        });
     };
 
     return {
